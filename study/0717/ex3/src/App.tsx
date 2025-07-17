fe8d98d909b9c2e7e156f440b6f8b2b3ed028a8f
hmjlon <jiyoung110077@gmail.com>
2025-07-17 16:09:15 +0900

    context 업데이트

---
+++ study/0717/ex3/src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "@fontsource/material-icons";
+import RefTest from "./pages/RefTest";
+import App2 from "./contexts/UserContext";
+import ThemeTest from "./pages/ThemeTest";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <RefTest />
+//     </div>
+//   );
+// }
+
+function App() {
+  return <ThemeTest />;
+}
+
+export default App;
