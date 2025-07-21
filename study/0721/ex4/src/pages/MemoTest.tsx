24b86ca472649a87021688e723ef5989c95c1a59
hmjlon <jiyoung110077@gmail.com>
2025-07-21 13:59:58 +0900

    UseCallback

--- study/0721/ex4/src/pages/MemoTest.tsx
+++ study/0721/ex4/src/pages/MemoTest.tsx
@@ -1,5 +1,7 @@
 import ExpensiveComponent from "../components/ExpensiveComponent";
 import UseMemoExample from "../components/UseMemoExample";
+import Parent from "../components/Parent";
+import UseCallbackExample from "../components/UseCallbackExample";
 
 export default function MemoTest() {
   return (
@@ -7,6 +9,8 @@ export default function MemoTest() {
       <h2>useMemo 테스트</h2>
       <ExpensiveComponent />
       <UseMemoExample />
+      <Parent />
+      <UseCallbackExample />
     </div>
   );
 }
