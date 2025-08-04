23db0ec214701d5008e181b79c21fdc6500e9797
hmjlon <jiyoung110077@gmail.com>
2025-08-05 00:57:00 +0900

    수정

--- study/0804/chapter2/hello.js
+++ study/0804/chapter2/hello.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   log(count);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.write("hello|n");
+  res.write("hello\n");
   setTimeout(() => {
     res.end("Node.js");
   }, 2000);
