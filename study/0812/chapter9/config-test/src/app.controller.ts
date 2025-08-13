f9c2072f45e0289620293c79e0dbd13cfd571a92
hmjlon <jiyoung110077@gmail.com>
2025-08-13 11:00:26 +0900

    chapter9

---
+++ study/0812/chapter9/config-test/src/app.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Controller()
+export class AppController {
+  constructor(private configService: ConfigService) {}
+
+  @Get()
+  getHello(): string {
+    const message = this.configService.get('MESSAGE');
+    return message;
+  }
+
+  @Get('service-url')
+  getServiceUrl(): string {
+    return this.configService.get('SERVICE_URL') ?? '';
+  }
+
+  @Get('server-url')
+  getServerUrl(): string {
+    return this.configService.get('SERVER_URL') ?? '';
+  }
+
+  @Get('db-info')
+  getTest(): string {
+    console.log(this.configService.get('logLevel'));
+    console.log(this.configService.get('apiVersion'));
+    return this.configService.get('dbInfo') ?? '';
+  }
+
+  @Get('redis-info')
+  getRedisInfo(): string {
+    const host = this.configService.get('redis.host') ?? '';
+    const port = this.configService.get('redis.port') ?? '';
+    return `${host}:${port}`;
+  }
+}
