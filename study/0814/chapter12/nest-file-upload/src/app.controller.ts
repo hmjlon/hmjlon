f92395a3c3f13a561162f63402c91a3624decf0c
hmjlon <jiyoung110077@gmail.com>
2025-08-14 14:49:51 +0900

    chpter12 파일 업로드 기능 구현

---
+++ study/0814/chapter12/nest-file-upload/src/app.controller.ts
@@ -0,0 +1,28 @@
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AppService } from './app.service';
+import { multerOption } from './multer.options';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('file-upload')
+  @UseInterceptors(FileInterceptor('file', multerOption))
+  fileUpload(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    // console.log(file.buffer.toString('utf-8'));
+    return `cat.jpg File Uploaded check http://localhost:3000/uploads/1ebf71cd-8a11-4cd6-a28a-14f54a46b97b.jpg`;
+  }
+}
